5458491474106288432d79010ebb329abe1e15b6
Ibrahimshaker1 <ibrahimshaker829@gmail.com>
2023-04-21 15:48:03 -0800

    update models.py#1

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+
 
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    publisher_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.publisher_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+# after user make a post where should he go for that he use get_absolute_url
+    def get_absolute_url(self):
+        # go to post_detail of the post that you just create
+        return reverse('post_detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=150)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approved(self):
+        self.approved_comment = True
+        self.save()
+
+# after use create a commit he will go to post_list page
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.text
