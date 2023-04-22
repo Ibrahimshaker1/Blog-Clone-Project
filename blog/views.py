3eb436afb7022301cbaec54fd940cb68f20415a5
Ibrahimshaker1 <ibrahimshaker829@gmail.com>
2023-04-21 17:53:47 -0800

    Update view.py#1

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
-
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
+from django.utils import timezone
+from django.urls import reverse_lazy
+from .forms import PostForm, CommentForm
+# LoginRequiredMixin this function use if you want something to view only if the use login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post, Comment
 # Create your views here.
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    # we can use get_queryset to make ListView return a specific objects form sql model and yo and order the list
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    # when use not login in go to /login/ and redirect hem to post_detail
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    # when delete is done go to post_list
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by("-created_date")
